a601b5fd7b89436e6e30b8e15cf39313fe5c16e8
TjandraD <tjandradarmo@protonmail.com>
2020-12-18 14:27:30 +0700

    Add animated icon in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sensors/sensors.dart';
+import '../models/weather_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   static String id = 'home_screen';
@@ -8,8 +13,75 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  AccelerometerEvent acceleration;
+  StreamSubscription<AccelerometerEvent> _streamSubscription;
+  Random random = Random();
+  int mainIcon = 0;
+  int iconMotionSensitivity = 4;
+
+  @override
+  void initState() {
+    super.initState();
+    pickIcon();
+    _streamSubscription = accelerometerEvents.listen((event) {
+      setState(() {
+        acceleration = event;
+      });
+    });
+  }
+
+  void pickIcon() {
+    // this function is supposed to match the real data if this app is using an API
+    mainIcon = random.nextInt(weatherIconList.length - 1);
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: screenHeight,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.7), BlendMode.dstATop),
+                    fit: BoxFit.fitHeight,
+                    image: AssetImage('assets/images/bg.png'),
+                  ),
+                ),
+              ),
+              AnimatedPositioned(
+                duration: Duration(milliseconds: 250),
+                top: acceleration.y * iconMotionSensitivity,
+                bottom: acceleration.y * -iconMotionSensitivity,
+                right: acceleration.x * -iconMotionSensitivity,
+                left: acceleration.x * iconMotionSensitivity,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: screenHeight / 6),
+                    child: SvgPicture.asset(
+                      'assets/svg/${weatherIconList[mainIcon]}',
+                      width: screenWidth - 200,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
